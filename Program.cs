399cb37dcf6b8f75073eaec37553293d32a4d633
Jahle <Jahle@ABLACKMAN>
2024-05-20 16:12:56 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Reflection.Metadata.Ecma335;
+
+//Counting String Spaces
+
+
+namespace Assignment_3._1._3
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Please input a string: ");
+            string input = Console.ReadLine();
+
+            int spaceCount = CountSpaces(input);
+
+            Console.WriteLine("\"" + input + "\" contains " + spaceCount + " spaces");
+        }
+        static int CountSpaces(string input)
+        {
+            int count = 0;
+
+            foreach (char c in input)
+            {
+                if (c == ' ')
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+    }
+}
